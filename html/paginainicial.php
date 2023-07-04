068888c40d08195180181a3b807b8c7b0e91d835
amandaraasch <amanda_raasch@estudante.sesisenai.org.br>
2023-07-04 10:56:47 -0300

    3 04/07

--- html/paginainicial.php
+++ html/paginainicial.php
@@ -34,7 +34,7 @@ while ($linhas = $comando->fetch())
     <div class="principal">
         
         <div class="D1">
-        <img id="status-img" src="../img/vermelha.png" alt="Offline" width="25px" height="25px">
+        <img id="status-img" src="../img/vermelha.png" alt="Offline" width="22px" height="22px">
             <div class="D2">
               
                 
